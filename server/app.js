b3d99074b9912c4306efb2372e167e4b26f9e1e0
Excomunicad0 <e_zhul@mail.ru>
2024-09-11 20:38:15 +0300

    feat/addMulter

--- server/app.js
+++ server/app.js
@@ -1,12 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 
-
 const serverConfig = require("./config/serverConfig");
 const indexRouter = require("./routes/index.routes");
-
-
-
 
 const PORT = process.env.PORT || 4000;
 const app = express();
