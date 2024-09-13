298fa0300b91437d6e942bea3a0d806087d5fd76
sadmaloy <rostislav_ivanov2015@mail.ru>
2024-09-13 10:10:52 +0300

    fix: style update in navbar

---
+++ client/src/pages/main/MainPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function MainPage() {
+  return (
+    <div>
+      <h1>Добро пожаловать в наш магазин чая!</h1>
+      
+    </div>
+  );
+}
+
+export default MainPage;
